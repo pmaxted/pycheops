becf18372506767b88b5e316b04f558425dc2316
pmaxted <p.maxted@keele.ac.uk>
2018-12-04 11:42:54 +0000

    pi

--- pycheops/models.py
+++ pycheops/models.py
@@ -143,8 +143,8 @@ class pModel(Model):
         if include_frozen:
             return self.parameter_priors
         return list(p
-                    for p, f in zip(self.parameter_priors, self.unfrozen_mask)
-                    if f)
+                for p, f in zip(self.parameter_priors, self.unfrozen_mask)
+                if f)
 
     def log_prior(self):
         """Compute the log prior probability of the current parameters"""
@@ -154,7 +154,7 @@ class pModel(Model):
             if b[1] is not None and p > b[1]:
                 return -np.inf
         lp = 0.0
-        for p,g  in zip(self.parameter_vector, self.parameter_priors):
+        for p,g in zip(self.parameter_vector, self.parameter_priors):
             if g[0] is not None and g[1] is not None:
                 lp -= 0.5*((p-g[0])/g[1])**2
 
@@ -189,40 +189,41 @@ def qpower2(z,p,c,a):
         warn ("qpower2 is untested/inaccurate for values of p > 0.2")
 
     f = np.ones_like(z)
-    I_0 = (a+2)/(pi*(a-c*a+2))
+    I_0 = (a+2)/(np.pi*(a-c*a+2))
     g = 0.5*a
     for i,zi in enumerate(z):
-        zt = abs(zi)
+        zt = np.abs(zi)
         if zt <= (1-p):
             s = 1-zt**2
             c0 = (1-c+c*s**g)
             c2 = 0.5*a*c*s**(g-2)*((a-1)*zt**2-1)
-            f[i] = 1-I_0*pi*p**2*(
+            f[i] = 1-I_0*np.pi*p**2*(
                     c0 + 0.25*p**2*c2 - 0.125*a*c*p**2*s**(g-1) )
-        elif abs(zt-1) < p:
+        elif np.abs(zt-1) < p:
             d = (zt**2 - p**2 + 1)/(2*zt)
             ra = 0.5*(zt-p+d)
             rb = 0.5*(1+d)
             sa = 1-ra**2
             sb = 1-rb**2
-            q = min(max(-1,(zt-d)/p),1)
+            q = np.min(np.max(-1,(zt-d)/p),1)
             w2 = p**2-(d-zt)**2
-            w = sqrt(w2)
+            w = np.sqrt(w2)
             b0 = 1 - c + c*sa**g
             b1 = -a*c*ra*sa**(g-1)
             b2 = 0.5*a*c*sa**(g-2)*((a-1)*ra**2-1)
             a0 = b0 + b1*(zt-ra) + b2*(zt-ra)**2
             a1 = b1+2*b2*(zt-ra)
-            aq = arccos(q)
+            aq = np.arccos(q)
             J1 = ( (a0*(d-zt)-(2/3)*a1*w2 + 
                 0.25*b2*(d-zt)*(2*(d-zt)**2-p**2))*w
                  + (a0*p**2 + 0.25*b2*p**4)*aq )
             J2 = a*c*sa**(g-1)*p**4*(
-                0.125*aq + (1/12)*q*(q**2-2.5)*sqrt(max(0,1-q**2)) )
+                0.125*aq + (1/12)*q*(q**2-2.5)*np.sqrt(np.max(0,1-q**2)) )
             d0 = 1 - c + c*sb**g
             d1 = -a*c*rb*sb**(g-1)
-            K1 = ((d0-rb*d1)*arccos(d) + 
-                    ((rb*d+(2/3)*(1-d**2))*d1 - d*d0)*sqrt(max(0,1-d**2)) )
+            K1 = ((d0-rb*d1)*np.arccos(d) + 
+                    ((rb*d+(2/3)*(1-d**2))*d1 - d*d0) * 
+                    np.sqrt(np.max(0,1-d**2)) )
             K2 = (1/3)*c*a*sb**(g+0.5)*(1-d)
             f[i] = 1 - I_0*(J1 - J2 + K1 - K2)
     return f
@@ -243,15 +244,15 @@ def ueclipse(z,p,f):
 
     fl = np.ones_like(z)
     for i,zi in enumerate(z):
-        zt = abs(zi)
+        zt = np.abs(zi)
         if zt <= (1-p):
             fl[i] = 1/(1+f)
-        elif abs(zt-1) < p:
+        elif np.abs(zt-1) < p:
             fl[i] = 1 - f/(1+f)*(p**2*
-                    arccos(min(max(-1,(zt**2+p**2-1)/(2*zt*p)),1)) + 
-                    arccos(min(max(-1,(zt**2+1-p**2)/(2*zt)),1)) - 
-                    0.5*sqrt(max(0,(1+p-zt)*(zt+p-1)*(zt-p+1)*(zt+p+1)))
-                    ) /(pi*p**2)
+                    np.arccos(np.min(np.max(-1,(zt**2+p**2-1)/(2*zt*p)),1)) + 
+                    np.arccos(np.min(np.max(-1,(zt**2+1-p**2)/(2*zt)),1)) - 
+                    np.sqrt(np.max(0,(1+p-zt)*(zt+p-1)*(zt-p+1)*(zt+p+1)))/2
+                    ) /(np.pi*p**2)
     return fl
 
 #class TransitModel(pModel):
