33a60a915934e055c084c7f717d4fef4e2508e92
Thomas Wilson <wilsont493@gmail.com>
2021-01-08 15:03:42 +0000

    deltaT reject_highpoints

--- pycheops/dataset.py
+++ pycheops/dataset.py
@@ -878,6 +878,7 @@ class Dataset(object):
             bg = bg[ok]
             contam = contam[ok]
             smear = smear[ok]
+            deltaT = deltaT[ok]
             N_cut = len(bjd) - len(time)
 
         fluxmed = np.nanmedian(flux)
